8a05a79197570f783e6cd1221dbba15fe5a3115c
kudachan <kumarnchandan05@gmail.com>
2025-03-31 14:25:12 +0530

    regular commit

--- JS_TS_DSA/JS/0_basic_problem/basic/01_check_even_or_odd.js
+++
@@ -1,9 +0,0 @@
-// Given a number n, check whether it is even or odd. Return true for the even and false for odd.
-
-function isEven(num) {
-  return num & 1 ? false : true;
-}
-
-for ( let i = 0; i <= 10; i++ ) {
-  console.log( isEven(i) );
-}
